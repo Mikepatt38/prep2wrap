67c9d29435600eb017aa69dfcbbc3713f52be971
Michael Patterson <michael@outlyrs.com>
2018-06-30 12:44:32 -0500

    Basic routes and routing implemented

---
+++ src/js/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li> 
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>     
+      </ul>
+    )
+  }
+}
+
+export default Navbar
--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -5,16 +5,27 @@ import {
   Switch
 } from 'react-router-dom'
 
+import Navbar from '../components/Navbar'
 import App from '../components/App'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
+
+import Dashboard from '../views/Dashboard'
 
 class Routes extends Component {
 
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path='/' component={App} />
-        </Switch>
+        <React.Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={App} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Switch>
+        </React.Fragment>
       </Router>
     )
   }
