e2f1a20b3592e0c29d2bf82d4fffe73780c1dd7f
Michael Patterson <michael@outlyrs.com>
2018-06-30 12:29:10 -0500

    Adding react router 4 setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styles from './styles/index.css'
-import App from './js/components/App'
+import App from './js/routes/routes'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
