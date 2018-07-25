530d41ca8d9c7788b1fa870b13db89bc5f7413b1
Michael Patterson <michael@outlyrs.com>
2018-07-25 14:11:16 -0500

    Re added sidebar component

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -3,36 +3,41 @@ import { connect } from 'react-redux'
 import AccountIcon from '../../img/icon-account.svg'
 import { Link } from 'react-router-dom'
 
+const tempStyle = {
+  paddingTop: '50px'
+}
+
 class Sidebar extends Component {
   render() {
     return (
-      <div className={this.props.authUser ? 'sidebar' : 'sidebar closed'}>
-        <ul className="sidebar-nav">
-          <li className="sidebar-nav-item">
-            <Link to="/dashboard">
-              <img src={AccountIcon} alt="Home Icon" />
-              Home
-            </Link>
-          </li>
-          <li className="sidebar-nav-item">
-            <a>
-              <img src={AccountIcon} alt="Jobs Icon" />
-              Jobs
-            </a>
-          </li>
-          <li className="sidebar-nav-item">
-            <Link to="/users">
-              <img src={AccountIcon} alt="Users Icon" />
-              Users
-            </Link>
-          </li>
-          <li className="sidebar-nav-item">
-            <a>
-              <img src={AccountIcon} alt="Availability Icon" />
-              Availability
-            </a>
-          </li>
-        </ul>
+      <div className="app-container-sidebar">
+        <div className="sidebar">
+          <div className="sidebar-section" style={tempStyle}>
+            <div>
+              <h5><Link to="/dashboard">Dashboard</Link></h5>
+            </div>
+          </div>
+          <div className="sidebar-section">
+            <div>
+              <h5><Link to="/dashboard">Jobs</Link></h5>
+            </div>
+          </div>
+          <div className="sidebar-section">
+            <div>
+              <h5><Link to="/users">Users</Link></h5>
+            </div>
+          </div>
+          <div className="sidebar-section">
+            <div>
+              <h5><Link to="/dashboard">Availability</Link></h5>
+            </div>
+          </div>
+          <div className="sidebar-section">
+            <div>
+              <h5><Link to="/account-settings">Account</Link></h5>
+            </div>
+          </div>
+        </div>    
       </div>
     )
   }
