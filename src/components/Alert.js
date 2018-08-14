a6d9c5b5821bff22db6f0b5bfc521301150bd9f9
Michael Patterson <michael@outlyrs.com>
2018-08-14 00:32:36 -0500

    Updated alert component to be leaner

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,15 +3,8 @@ import PropTypes from 'prop-types'
 
 export const Alert = ({ active, alertText, alertType, onSetAlert }) => {
   return (
-    <div className={active ? 'alert active ' + `${alertType}` : 'alert'}>
-      <div className="alert-info">
-        <div className="alert-text">
-          <p>{alertText}</p>
-        </div>
-        <div className="alert-close" onClick={ () => onSetAlert(false, '', '') }>
-          <p>X</p>
-        </div>
-      </div>
+    <div className={active ? 'alert active ' + `${alertType}` : 'alert'} onClick={ () => onSetAlert(false, '', '') }>
+      <p>{alertText}</p>
     </div>
   )
 }
@@ -19,5 +12,6 @@ export const Alert = ({ active, alertText, alertType, onSetAlert }) => {
 Alert.propTypes = {
   active: PropTypes.bool.isRequired,
   alertText: PropTypes.string.isRequired,
-  alertType: PropTypes.string.isRequired
+  alertType: PropTypes.string.isRequired,
+  onSetAlert: PropTypes.func
 }
