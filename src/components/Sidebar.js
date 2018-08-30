b45301dbeb610d4f48fde7352e868c27c4be56a1
Michael Patterson <michael@outlyrs.com>
2018-08-29 20:52:13 -0500

    Updated sidebar UI and added icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
-const tempStyle = {
-  paddingTop: '50px'
-}
+import logo from '../img/calltime-logo.png'
+import DashboardIcon from '../img/icon-dashboard.svg'
+import JobsIcon from '../img/icon-jobs.svg'
+import UsersIcon from '../img/icon-users.svg'
+import AvailabilityIcon from '../img/icon-availability.svg'
+import AccountIcon from '../img/icon-settings.svg'
+import LogoutIcon from '../img/icon-logout.svg'
 
 class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
-        <div className="sidebar-section" style={tempStyle}>
+        <div className="logo-wrapper">
+          <a href="/"></a>
+        </div>
+        <div className="sidebar-section">
           <div>
-            <h5><Link to="/dashboard">Dashboard</Link></h5>
+            <h5><img src={DashboardIcon} /><Link to="/dashboard">Dashboard</Link></h5>
           </div>
         </div>
         <div className="sidebar-section">
           <div>
-            <h5><Link to="/dashboard">Jobs</Link></h5>
+            <h5><img src={JobsIcon} /><Link to="/dashboard">Hire A Crew</Link></h5>
           </div>
         </div>
         <div className="sidebar-section">
           <div>
-            <h5><Link to="/users">Users</Link></h5>
+            <h5><img src={UsersIcon} /><Link to="/users">Call Time Users</Link></h5>
           </div>
         </div>
         <div className="sidebar-section">
           <div>
-            <h5><Link to="/availability">Availability</Link></h5>
+            <h5><img src={AvailabilityIcon} /><Link to="/availability">My Availability</Link></h5>
           </div>
         </div>
         <div className="sidebar-section">
           <div>
-            <h5><Link to="/account-settings">Account</Link></h5>
+            <h5><img src={AccountIcon} /><Link to="/account-settings">My Account</Link></h5>
+          </div>
+        </div>
+        <div className="sidebar-section sidebar-section--bottom">
+          <div>
+            <h5><img src={LogoutIcon} /><Link to="/account-settings">Logout</Link></h5>
           </div>
         </div>
       </div> 
