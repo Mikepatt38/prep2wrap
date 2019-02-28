8c7d28571d82517f8b2627d8b4dac21fccc73b20
Michael Patterson <michael@outlyrs.com>
2019-02-28 13:56:20 -0600

    Updated final styling for login view

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { FormTextInput } from '../Forms/FormTextInput'
 import { FormButton } from '../Forms/FormButton'
+import logo from '../../img/calltime-logo.png'
 import LoginIllustration from '../../img/illustration-login.svg'
 
 export class Login extends Component {
@@ -19,41 +20,21 @@ export class Login extends Component {
   }
 
   render() {
-    const { history, signUserIn, logo } = this.props
+    const { history, signUserIn } = this.props
     return (
-      <div className="page-background">
-        <div className="modal authModal">
-          <div className="modalIllustration">
-            <div className="illustration">
-              <img src={LoginIllustration} alt="Login Illustration" />
-            </div>
-            <div className="illustration-text">
-              <h3>Grow your career and network.</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-            </div>
+      <div className="authPage">
+        <div className="auth-container">
+          <div className="auth-logo">
+            <img src={logo} alt="The official logo" />
           </div>
-          <div className="modalFormContainer">
-            <div className="modalForm">
-              <div className="modal-header">
-                <h2>Hey, welcome back!</h2>
-                <p>Login and get started with your network.</p>
-              </div>
-              <div className="modal-body">
-                <LoginForm
-                  history={history}
-                  signUserIn={signUserIn}
-                  state={this.state}
-                  handleChange={this.handleChange}
-                />
-              </div>
-              <div className="modal-footer">
-                <ForgotPassword />
-                <hr />
-                <p className="info">Not a member yet?</p>
-                <Link to="/signup" className="link">
-                  Sign up now to get started
-                </Link>
-              </div>
+          <div className="auth-card">
+            <div className="auth-card-body">
+              <LoginForm
+                history={history}
+                signUserIn={signUserIn}
+                state={this.state}
+                handleChange={this.handleChange}
+              />
             </div>
           </div>
         </div>
@@ -61,6 +42,44 @@ export class Login extends Component {
     )
   }
 }
+
+// <div className="page-background">
+// <div className="modal authModal">
+//   <div className="modalIllustration">
+//     <div className="illustration">
+//       <img src={LoginIllustration} alt="Login Illustration" />
+//     </div>
+//     <div className="illustration-text">
+//       <h3>Grow your career and network.</h3>
+//       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+//     </div>
+//   </div>
+//   <div className="modalFormContainer">
+//     <div className="modalForm">
+//       <div className="modal-header">
+//         <h2>Hey, welcome back!</h2>
+//         <p>Login and get started with your network.</p>
+//       </div>
+//       <div className="modal-body">
+//         <LoginForm
+//           history={history}
+//           signUserIn={signUserIn}
+//           state={this.state}
+//           handleChange={this.handleChange}
+//         />
+//       </div>
+//       <div className="modal-footer">
+//         <ForgotPassword />
+//         <hr />
+//         <p className="info">Not a member yet?</p>
+//         <Link to="/signup" className="link">
+//           Sign up now to get started
+//         </Link>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </div>
 
 const LoginForm = ({ history, signUserIn, state, handleChange}) => (
   <form onSubmit={(e) => signUserIn(state.email, state.password, history, e)}> 
--- src/pages/UserAuth.js
+++ src/pages/UserAuth.js
@@ -6,78 +6,25 @@ import { Login } from '../components/Auth/Login'
 import { SignUp } from '../components/Auth/SignUp'
 import { PasswordReset } from '../components/Auth/PasswordReset'
 
-const AuthNav = ({ currentUser }) => (
-  <div className="header auth" id="dashboard">
-    <div className="header-section">
-      <div className="grid-item">
-        <a className="logo"><img src={logo} alt="Calltime Dashboard Logo" /></a>
-      </div>
-    </div>
-    <div className="header-section"></div>
-    <div className="header-section">
-      <div className="grid-item grid-item--right">
-        <ul className="nav nav--dark">
-          {
-            currentUser === null 
-            ?
-            <React.Fragment>
-              <li>
-                <span><Link to="/login">Login</Link></span>
-              </li>
-              <li>
-                <span><Link to="/signup">SignUp</Link></span>
-              </li>
-            </React.Fragment>
-            :
-            <React.Fragment>
-              <li>
-                <span><Link to="/login">Support</Link></span>
-              </li>
-            </React.Fragment>
-          }
-        </ul>
-      </div>
-    </div>
-  </div>
-)
-
-const AuthFooter = () => (
-  <footer className="footer--grey">
-    <p>Copyright The Calltime &copy; {dateFns.format(new Date(), "YYYY")}</p>
-  </footer>
-)
-
 const UserAuth = ({ history, signUserIn, signUpUser, resetPassword, location, currentUser, setUserProfile }) => {
   switch(location.pathname) {
     case '/login':
       return (
-        <React.Fragment>
-          <AuthNav
-            currentUser={currentUser}
-          />
-          <Login 
-            signUserIn={signUserIn}
-            history={history}
-          />
-          <AuthFooter />
-        </React.Fragment>
+        <Login 
+          signUserIn={signUserIn}
+          history={history}
+        />
       )
       break
     
     case '/signup':
       return (
-        <React.Fragment>
-          <AuthNav
-            currentUser={currentUser}
-          />
-          <SignUp 
-            signUpUser={signUpUser}
-            history={history} 
-            currentUser={currentUser}
-            setUserProfile={setUserProfile}
-          />
-          <AuthFooter />
-        </React.Fragment>
+        <SignUp 
+          signUpUser={signUpUser}
+          history={history} 
+          currentUser={currentUser}
+          setUserProfile={setUserProfile}
+        />
       )
       break
 
