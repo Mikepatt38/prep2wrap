a2834ccfeb8d5f5296d463b22ee3e4ef7a687e09
Michael Patterson <michael@outlyrs.com>
2019-03-01 00:07:56 -0600

    dashboard page styles complete for now

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import withAuthorization from '../containers/withAuthorization'
-import DashboardNotifications from '../components/General/DashboardNotifications';
+import DashboardNotifications from '../components/General/DashboardNotifications'
+import GettingStartedIcon from '../img/icon-getting-started.svg'
+import CreateIcon from '../img/icon-create.svg'
+import GlassIcon from '../img/icon-glass.svg'
+import CalendarIcon from '../img/icon-calendar.svg'
 
 class Dashboard extends Component {
   render() {
@@ -15,6 +19,45 @@ class Dashboard extends Component {
           getUserJobNotifications={this.props.getUserJobNotifications}
           removeUserJobNotification={this.props.removeUserJobNotification}
         />
+
+        <div className="app-page-section">
+          <div className="section-title">
+            <h3>Quick Actions</h3>
+          </div>
+          <div className="dashboard-cards">
+            <div className="dashboard-card">
+              <div className="icon"><img src={GettingStartedIcon} alt="Getting Started Icon" /></div>
+              <div className="description">
+                <h4>Getting Started</h4>
+                <p>Get started with how to use the Calltime platform for your career and current jobs.</p>
+              </div>
+            </div>
+
+            <div className="dashboard-card">
+              <div className="icon"><img src={CreateIcon} alt="Getting Started Icon" /></div>
+              <div className="description">
+                <h4>Create A Job</h4>
+                <p>Get started with how to use the Calltime platform for your career and current jobs.</p>
+              </div>
+            </div>
+
+            <div className="dashboard-card">
+              <div className="icon"><img src={GlassIcon} alt="Getting Started Icon" /></div>
+              <div className="description">
+                <h4>Update Availability</h4>
+                <p>Get started with how to use the Calltime platform for your career and current jobs.</p>
+              </div>
+            </div>
+
+            <div className="dashboard-card">
+              <div className="icon"><img src={CalendarIcon} alt="Getting Started Icon" /></div>
+              <div className="description">
+                <h4>Find A User</h4>
+                <p>Get started with how to use the Calltime platform for your career and current jobs.</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
