35f1d39202556c77feb7714babdd29a91b4cb867
Michael Patterson <michael@outlyrs.com>
2019-06-29 04:42:12 -0500

    Updated user dashboard design and layout

--- src/components/General/DashboardStatus.js
+++ src/components/General/DashboardStatus.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class DashboardStatus extends Component {
   state = {
@@ -22,24 +23,81 @@ class DashboardStatus extends Component {
   render() {
     if(this.state.loading) return <p>Loading...</p>
     return (
-      <React.Fragment>
-        <div className="dashboard-status-cards">
-          <div className="status-card">
-            <span className="count">{this.state.createdJobsCount}</span>
-            <span className="description">Jobs<br/>Created</span>
-          </div>
-          <div className="status-card">
-            <span className="count">{this.state.acceptedJobsCount}</span>
-            <span className="description">Jobs<br/>Accepted</span>
-          </div>
-          <div className="status-card">
-            <span className="count">{this.state.completedJobsCount}</span>
-            <span className="description">Jobs<br/>Completed</span>
+      <div className="app-page-section" id="section-grid">
+        <div className="card card-grid">
+          <div className="card-body">
+            <h4>Jobs Overview</h4>
+            <p>Here is a quick overview of the jobs you have created, completed, and accepted to be apart of.</p>
+            <div className="dashboard-status-cards">
+              <div className="status-card">
+                <span className="count">{this.state.createdJobsCount}</span>
+                <span className="description">Jobs Created</span>
+              </div>
+              <div className="status-card">
+                <span className="count">{this.state.acceptedJobsCount}</span>
+                <span className="description">Jobs Accepted</span>
+              </div>
+              <div className="status-card">
+                <span className="count">{this.state.completedJobsCount}</span>
+                <span className="description">Jobs Completed</span>
+              </div>
+            </div>
           </div>
         </div>
-      </React.Fragment>
+        <div className="card card-grid card-grid--vertical">
+          <div className="card-body">
+            <div className="card-body-header">
+              <h4>Daily Availability</h4>
+              <Link to="/availability" className="button">Manage Schedule</Link>
+            </div>
+            <p>Easily update your daily availability to keep crew members up to date with your schedule.</p>
+            <div className="checkbox-reg-wrapper">
+              <input 
+                type="checkbox" 
+                id="availability-checkbox" 
+                // onChange={onChange} 
+                // value={value} 
+                checked={false}
+              />
+              <label className="checkbox-regular" htmlFor="availability-checkbox">I am unavailable today.</label>
+            </div>
+          </div>
+        </div>
+        <div className="card card-grid card-grid--vertical">
+          <div className="card-body">
+            <div className="card-body-header">
+              <h4>Quick Crew</h4>
+              <Link to="/users" className="button">Search Crew</Link>
+            </div>
+            <p>Keep track of your quick crew to continue to grow your network.</p>
+            <div className="dashboard-status-cards">
+              <div className="status-card status-card-lg">
+                <span className="count">{this.props.currentUser.favorites.length}</span>
+                <span className="description">Quick Crew Members</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
 export default DashboardStatus
+
+// <React.Fragment>
+// <div className="dashboard-status-cards">
+//   <div className="status-card">
+//     <span className="count">{this.state.createdJobsCount}</span>
+//     <span className="description">Jobs<br/>Created</span>
+//   </div>
+//   <div className="status-card">
+//     <span className="count">{this.state.acceptedJobsCount}</span>
+//     <span className="description">Jobs<br/>Accepted</span>
+//   </div>
+//   <div className="status-card">
+//     <span className="count">{this.state.completedJobsCount}</span>
+//     <span className="description">Jobs<br/>Completed</span>
+//   </div>
+// </div>
+// </React.Fragment>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -22,51 +22,10 @@ class Dashboard extends Component {
             removeUserJobNotification={this.props.removeUserJobNotification}
           />
 
-          <div className="app-page-section">
-            <DashboardStatus
-              currentUser={this.props.currentUser}
-              getUserJobCount={this.props.getUserJobCount}
-            />
-          </div>
-          <br />
-          <div className="app-page-section">
-            <div className="section-title">
-              <p>Quick actions:</p>
-            </div>
-            <div className="dashboard-cards">
-              <div className="dashboard-card">
-                <div className="icon"><img src={GettingStartedIcon} alt="Getting Started Icon" /></div>
-                <div className="description">
-                  <h4>Visit Tutorials</h4>
-                  <p>Learn the Crew It Up platform.</p>
-                </div>
-              </div>
-
-              <div className="dashboard-card">
-                <div className="icon"><img src={CreateIcon} alt="Getting Started Icon" /></div>
-                <div className="description">
-                  <h4>Create A Job</h4>
-                  <p>Hire your next crew.</p>
-                </div>
-              </div>
-
-              <div className="dashboard-card">
-                <div className="icon"><img src={GlassIcon} alt="Getting Started Icon" /></div>
-                <div className="description">
-                  <h4>Update Availability</h4>
-                  <p>Keep your calendar updated.</p>
-                </div>
-              </div>
-
-              <div className="dashboard-card">
-                <div className="icon"><img src={CalendarIcon} alt="Getting Started Icon" /></div>
-                <div className="description">
-                  <h4>Find A User</h4>
-                  <p>Easily grow your network.</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <DashboardStatus
+            currentUser={this.props.currentUser}
+            getUserJobCount={this.props.getUserJobCount}
+          />
         </div>
       </div>
     )
