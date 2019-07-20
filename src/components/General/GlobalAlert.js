3e4b07a3954c6dd47f5d33a7b4704b66823f653d
Michael Patterson <michael@outlyrs.com>
2019-07-20 13:31:30 -0500

    Fixed global alert styling and functionality bugs

--- src/components/General/GlobalAlert.js
+++ src/components/General/GlobalAlert.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
-import CloseIcon from '../../img/icon-close.svg'
+import CloseIcon from '../../img/icon-close-white.svg'
 
 export function GlobalAlert(props){
   return (
@@ -9,8 +9,7 @@ export function GlobalAlert(props){
     ReactDOM.createPortal(
       <div className="global-alert active">
         <Link to="/account-settings">
-          <p><b>ALERT:</b> &nbsp;Complete your profile to send and receive jobs. Click to go to settings to update.</p>
-          <span onClick={ () => props.onSetAlert(false, '', '') }><img src={CloseIcon} alt="Close Icon" /></span>
+          <b>ALERT:</b> &nbsp;Click to update and complete your profile to send and receive jobs.
         </Link>
       </div>, document.body
     ) : null
