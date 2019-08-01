c1d0c7b36c26ee8e89e66744c383ec0092fd31cc
Michael Patterson <michael@outlyrs.com>
2019-08-01 00:48:07 -0500

    Added 404 page and updated the walkthrough screenshots with updated UI design

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import EmptyState from '../components/General/EmptyState'
+import NoResultsIllustration from '../img/illustrations/no-results.svg'
 
 export const NotFound = () => (
   <div className="app-page">
@@ -10,13 +12,18 @@ export const NotFound = () => (
           <Link to="/availability" className="link">Availability</Link>
           <Link to="/jobs" className="link">Jobs</Link>
           <Link to="/crew" className="link">Crew</Link>
-          <Link to="/account-settings" className="active">Settings</Link>
+          <Link to="/account-settings">Settings</Link>
         </div>
       </div>
     </div>
     <div className="app-page-body">
       <div className="app-page-section">
-        <p>Looks like that page doesn't exist</p>
+        <EmptyState
+          imgSrc={NoResultsIllustration}
+          imgAlt="Page Not Found Illustration"
+          title="That page does not exist."
+          text="The page you are looking for does not exist, use the navbar to navigate to a known section of the application."
+        />
       </div>
     </div>
   </div>
